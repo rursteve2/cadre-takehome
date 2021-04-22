7b8a0af2d22f2601b26d48f5eafc18908cf9b8ec
Stephen <rursteve2@yahoo.com>
2021-04-22 01:34:30 -0400

    added high and low temps

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Today(props) {
-    const {geocode, today} = props    
+    const {geocode, today} = props
+    const [high, setHigh] = useState(-Infinity)
+    const [low, setLow] = useState(Infinity)
+    const [current, setCurrent] = useState()
+
+    useEffect(() => {
+        if(today) {
+            setHighandLow()
+        }
+    }, [today])
+    const setHighandLow = () => {
+        setCurrent(today[0].temperature)
+        let tempHigh = today[0].temperature
+        let tempLow = today[0].temperature
+        for(let i = 0; i < 23; i++) {
+            if(today[i].temperature > tempHigh) {
+                tempHigh = today[i].temperature
+            }
+            if(today[i].temperature < tempLow) {
+                tempLow = today[i].temperature
+            }
+        }
+        setHigh(tempHigh)
+        setLow(tempLow)
+    }
     return (
         <div>
             <h3>Current Weather in {geocode.label}</h3>
+            <p>Current temp: {current} F</p>
+            <div>
+                <h3 className="titletemp">Next 24 hours:</h3>
+                <p className="temp">High: {high}</p>
+                <p className="temp">Low: {low}</p>
+            </div>
             {today ?
             <div className="weatherContainer">
                 {today[0] && today.map((item, key) => (
